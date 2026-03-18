b101d5c01d3f22694aabf5c16755e48d02f39f0d
LUCAS DIAS BRANDAO KOLLE <25203704@CFP127ALUNO.LOCAL>
2026-03-18 11:29:15 -0300

    finalizado por hoje

---
+++ modulo/functions.js
@@ -0,0 +1,15 @@
+/*****************************************************************************************************************************************************************************
+ * Objetivo: Arquivo para guardar as funções para manipular os dados em ARRAY e JASON
+ * Data: 18/03/2026
+ * Autor: Lucas Kolle
+ * Versão: 1.0.3.26
+ ****************************************************************************************************************************************************************************/
+
+//importando arquivo com as informações necessárias
+const estadosCidades = require("./estados_cidades.js")
+
+//função que retorna a lista de todos os estados do brasil
+const getListaDeEstados = function(){
+    
+}
+console.log(getListaDeEstados())
