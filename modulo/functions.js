bae8009418fac140f3006cefa2d318948affe10a
LUCAS DIAS BRANDAO KOLLE <25203704@CFP127ALUNO.LOCAL>
2026-03-20 07:46:52 -0300

    primeira função finalizada

--- modulo/functions.js
+++ modulo/functions.js
@@ -8,8 +8,18 @@
 //importando arquivo com as informações necessárias
 const estadosCidades = require("./estados_cidades.js")
 
-//função que retorna a lista de todos os estados do brasil
+//função que retorna a lista de todos as siglas dos estados do brasil
 const getListaDeEstados = function(){
-    
+    let siglaEstados = []
+
+    estadosCidades.listaDeEstados.estados.forEach(function(itemEstados){
+        siglaEstados.push(itemEstados.sigla)
+    })
+
+    if(siglaEstados){
+        return siglaEstados
+    }else{
+        return false
+    }
 }
 console.log(getListaDeEstados())
